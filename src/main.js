52c22981a6b1579eb3f8f2cb46084326d042cfda
gaomingli <gaomingli@qq.com>
2019-08-31 20:16:20 +0800

    页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+
 import './style/index.less'
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
