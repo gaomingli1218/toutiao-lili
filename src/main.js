a7d5bf16bc645b37313be1702c18f35159848dbc
gaomingli <gaomingli@qq.com>
2019-08-31 19:48:28 +0800

    整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import './style/index.less'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
